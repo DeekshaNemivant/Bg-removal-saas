582307f554a64cc1088620856ec20144bcde1768
waibaBikash <kavrelibikash@gmail.com>
2024-10-24 21:46:30 +1100

    create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import BuyCredit from './pages/BuyCredit';
 import Result from './pages/Result';
+import Navbar from './components/Navbar';
 
 
 const App = () => {
   return (
-    <div>
+    <div className='min-h-screen bg-slate-50'>
+      <Navbar/>
        <Routes>
          <Route path='/' element={<Home/>}/>
          <Route path='/result' element={<Result/>}/>
