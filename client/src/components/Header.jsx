90b936dcde17fa73685a8d87edfcacaf61e0720c
waibaBikash <kavrelibikash@gmail.com>
2024-11-20 20:22:32 +1100

    commit change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,10 +3,17 @@ import {assets} from '../assets/assets'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
+    <div 
+       className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
        {/* Left Side */}
        <div className=''>
-         <h1 className='text-4xl xl:text-5xl  2xl:text-6xl font-bold text-neutral-700 leading-tight'>Remove the <br className='max-md:hidden' /> <span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent '>background </span>from <br className='max-md:hidden' /> images for free.</h1>
+         <h1 
+          className='text-4xl xl:text-5xl  2xl:text-6xl font-bold text-neutral-700 leading-tight'>
+            Remove the <br className='max-md:hidden' />
+             <span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent '>background 
+               </span>from 
+               <br className='max-md:hidden' /> images for free.
+          </h1>
          <p className='my-6 text-[15px] text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quia <br className='max-sm:hidden'/> quibusdam soluta amet libero ipsum earum repellat, ab voluptatum nulla!</p>
           <div>
              <input type="file" name='' id='upload1' hidden />
