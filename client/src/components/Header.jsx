d27b5617c6f7f6c2912f170f1b2882fe92b7fce8
waibaBikash <kavrelibikash@gmail.com>
2024-11-20 20:23:40 +1100

    changes made

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,18 +10,21 @@ const Header = () => {
          <h1 
           className='text-4xl xl:text-5xl  2xl:text-6xl font-bold text-neutral-700 leading-tight'>
             Remove the <br className='max-md:hidden' />
-             <span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent '>background 
+             <span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent '>
+               background 
                </span>from 
                <br className='max-md:hidden' /> images for free.
           </h1>
          <p 
           className='my-6 text-[15px] text-gray-500'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quia
-             <br className='max-sm:hidden'/> quibusdam soluta amet libero ipsum earum repellat, ab voluptatum nulla!
+             <br className='max-sm:hidden'/> 
+             quibusdam soluta amet libero ipsum earum repellat, ab voluptatum nulla!
              </p>
           <div>
              <input type="file" name='' id='upload1' hidden />
-              <label className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700' htmlFor="upload1">
+              <label 
+              className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700' htmlFor="upload1">
                  <img width={20} src={assets.upload_btn_icon} alt="" />
                  <p className='text-white text-sm'>Upload your image</p>
               </label>
