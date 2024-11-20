a54a4afd6b139cf427f29f0aabb65295100ae4d5
waibaBikash <kavrelibikash@gmail.com>
2024-11-20 20:24:24 +1100

    save change

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -4,7 +4,8 @@ import { assets } from '../assets/assets'
 const Steps = () => {
   return (
     <div className='mx-4 lg:mx-44 py-20 xl:py-40'>
-      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent'>
+      <h1
+       className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent'>
         Steps to remove background <br /> image in seconds
       </h1>
        <div className='flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center'>
@@ -26,7 +27,8 @@ const Steps = () => {
              <img className='max-w-9' src={assets.download_icon} alt="" />
               <div>
                 <p className='text-xl font-medium'>Download image</p>
-                <p className='text-sm text-neutral-500 mt-1 '>This is a demo, will repalce it later. <br />This is a demo..</p>
+                <p className='text-sm text-neutral-500 mt-1 '>
+                  This is a demo, will repalce it later. <br />This is a demo..</p>
               </div>
           </div>
        </div>
