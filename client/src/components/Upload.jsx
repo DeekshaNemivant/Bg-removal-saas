b80716d9585df54bc3a99bd41d89f404fb17d6e7
waibaBikash <kavrelibikash@gmail.com>
2024-11-23 23:05:23 +1100

    save change

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -4,7 +4,10 @@ import { assets } from '../assets/assets'
 const Upload = () => {
   return (
     <div className='pb-16'>
-      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent py-6 md:py-16'>See the magic. Try now</h1>
+      <h1
+       className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent py-6 md:py-16'>
+        See the magic. Try now
+       </h1>
       <div className='text-center mb-24'>
              <input type="file" name='' id='upload2' hidden />
               <label className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700' htmlFor="upload2">
