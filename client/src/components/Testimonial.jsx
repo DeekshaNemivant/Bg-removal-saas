f7d510eeee58059cc30c7bef5042c16d30da9cd2
waibaBikash <kavrelibikash@gmail.com>
2024-11-24 22:08:11 +1100

    commit change

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -5,7 +5,10 @@ const Testimonial = () => {
   return (
     <div >
       {/* Title */}
-      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent py-5'>Customer Testimonials</h1>
+      <h1 
+       className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent py-5'>
+        Customer Testimonials
+       </h1>
        <div className='grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto px-4 py-8'>
         {testimonialsData.map((item,index)=>(
           <div className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700 cursor-pointer' key={index}>
