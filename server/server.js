ab0913a0473828331ff9f63798c6308e86a03391
waibaBikash <kavrelibikash@gmail.com>
2024-11-26 22:47:21 +1100

    create a user modles

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './configs/mongodb';
+import connectDB from './configs/mongodb.js';
 
 
 // App Config
