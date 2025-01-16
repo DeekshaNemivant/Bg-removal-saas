bbb73edc821d543f9e3dbb296860e368316dc234
waibaBikash <kavrelibikash@gmail.com>
2025-01-16 21:29:20 +1100

    save change

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -8,7 +8,10 @@ const BuyCredit = () => {
       className='border border-gray-400 px-10 py-2 rounded-full mb-6'>
         Our Plans
         </button>
-     <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent mb-6 sm:mb-10'>Choose the plan that's right for you</h1>
+     <h1 
+      className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-500 to-gray-400 bg-clip-text text-transparent mb-6 sm:mb-10'>
+        Choose the plan that's right for you
+        </h1>
       <div className='flex flex-wrap justify-center gap-6 text-left'>
         {plans.map((item,index)=>(
           <div className='bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-700 hover:scale-105 transition-all from-neutral-500' key={index}>
